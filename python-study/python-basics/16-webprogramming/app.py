5e52bbb654f75454d78bce8b701a3509b5e389a0
buwenzheng <>
2018-01-24 18:43:05 +0800

    study use webframe

---
+++ python-study/python-basics/16-webprogramming/app.py
@@ -0,0 +1,27 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+@app.route('/signin', methods=['GET'])
+def signin_from():
+    return render_template('form.html')
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    # 需要从request对象读取表单内容：
+    print(request.form['username'])
+    username = request.form['username']
+    password = request.form['password']
+    if username == 'admin' and password == '12345':
+        return render_template('signin-ok.html', username=username)
+    return render_template('form.html', message='Bad username or password!', username=username)
+
+if __name__ == '__main__':
+    app.run()
